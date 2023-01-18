698354ae0f50baacd5d1babf74408d9586cc5aff
taeng0204 <taeng02@icloud.com>
2023-01-18 21:52:49 +0900

    [2023.01.18] Add Main & Cat eyes

--- js/background.js
+++ js/background.js
@@ -1,15 +1,23 @@
 const theme = document.querySelector(':root');
 const styles = getComputedStyle(theme);
-const modeBtn = document.querySelector('header');
+const modeBtn = document.getElementById('modeBtn');
+const catName = document.getElementById('name');
+
+if(catName) catName.innerText = "둘째 고양이\n새미";
 
 const changeTheme = () => {
   const temp = styles.getPropertyValue('--main-bg-color');
   const text = {"White": "Dark", "Dark": "White"}
-  const eyeColor = {"#D7DF01": "yellowgreen", "yellowgreen": "#D7DF01"}
+  const eyeColor = {"#F4FA58": "yellowgreen", "yellowgreen": "#F4FA58"};
+  const name = {
+    "둘째 고양이\n새미" : "첫째 고양이\n까미",
+    "첫째 고양이\n까미" : "둘째 고양이\n새미"
+  }
   theme.style.setProperty('--main-bg-color', styles.getPropertyValue('--main-color'));
   theme.style.setProperty('--main-color', temp);
   theme.style.setProperty('--cat-eye-color', eyeColor[styles.getPropertyValue('--cat-eye-color').trim()]);
- 
+  
+  if(catName) catName.innerText = name[catName.innerText];
   modeBtn.innerText = text[modeBtn.innerText];
 }
 
