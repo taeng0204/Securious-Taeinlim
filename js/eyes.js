bdd64d23daa141e2827e59319a63d7f27eae1c5a
taeng0204 <taeng02@icloud.com>
2023-02-02 17:47:58 +0900

    [2023.02.02] Add Cat Wink

--- js/eyes.js
+++ js/eyes.js
@@ -1,4 +1,6 @@
 const eyes = document.getElementsByClassName("eye");
+const face = document.querySelector("main")
+
 document.onmousemove = function (event) {
   let x = event.clientX * 100 / (window.innerWidth - 180) / 2 + "%";
   let y = event.clientY * 100 / (window.innerHeight - 90)/ 2 + "%";
@@ -8,3 +10,17 @@ document.onmousemove = function (event) {
     eyes[i].style.top = y;
   }
 }
+
+const wink = () => {
+  for (let i = 0; i < 2; i++) {
+    eyes[i].style.height = '10px';
+  }
+  setTimeout(() => {
+    for (let i = 0; i < 2; i++) {
+      eyes[i].style.height = '40px';
+    }
+  }, 200);
+}
+
+console.log(face);
+face.addEventListener("click", wink)
